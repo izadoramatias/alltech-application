02083273a275a108d7dcc2e683f5bacd6114ead1
izadora matias <contato.izadoramatias@gmail.com>
2023-10-12 22:53:33 -0300

    updated renderLoginForm and created processLoginRequest

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,15 +2,25 @@
 
 namespace App\Controller;
 
+use App\DTO\LoginDTO;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login', methods: ['GET'])]
-    public function renderLoginPage(): Response
+    #[Route('/login', name: 'app_login_render', methods: ['GET'])]
+    public function renderLoginForm(): Response
     {
         return $this->render('login.html.twig');
+    }
+
+    #[Route('/login', name: 'app_login_request_process')]
+    public function processLoginRequest(Request $request): Response
+    {
+        $login = LoginDTO::fromRequest($request);
+
+        return new Response('ok');
     }
 }
