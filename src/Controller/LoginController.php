a3a883db0aab87d1eb0fbdb75db9d288600004c7
izadora matias <contato.izadoramatias@gmail.com>
2023-10-15 17:03:35 -0300

    updated processLoginRequest and created jsonSerialize

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\DTO\LoginDTO;
+use App\Entity\User;
+use App\Service\AuthService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,10 +19,26 @@ class LoginController extends AbstractController
     }
 
     #[Route('/login', name: 'app_login_request_process')]
-    public function processLoginRequest(Request $request): Response
+    public function processLoginRequest(
+        Request $request,
+        AuthService $auth
+    )
     {
         $login = LoginDTO::fromRequest($request);
+        $loginData = $this->jsonSerialize($auth->login($login));
 
-        return new Response('ok');
+        return $this->render('userOrderList.html.twig');
+    }
+
+
+    public function jsonSerialize(User $user)
+    {
+        $userArray = [
+            'name' => $user->getFullName(),
+            'email' => $user->getEmail(),
+            'permission' => $user->getPermission()
+        ];
+
+        return json_encode($userArray);
     }
 }
