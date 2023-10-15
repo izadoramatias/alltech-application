66a03a59e60694f62ccfdac979c2c07cff54a98d
izadora matias <contato.izadoramatias@gmail.com>
2023-10-15 16:57:58 -0300

    created AuthService

---
+++ src/Service/AuthService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Service;
+
+use App\DTO\LoginDTO;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+
+class AuthService
+{
+    public function __construct(
+        private UserRepository $userRepository
+    ){}
+
+    public function login(LoginDTO $login)
+    {
+        $login = $this->userRepository->findBy(['email' => $login->getEmail()]);
+
+        if (empty($login)) {
+            throw new BadRequestException('Login informado não existe');
+        }
+        return $login[0];
+    }
+}
