0d319ed93a2b57e24d2afd47ceda8b7833dab03f
izadora matias <contato.izadoramatias@gmail.com>
2023-10-19 23:19:52 -0300

    add login authentication verifier by password

--- src/Service/AuthService.php
+++ src/Service/AuthService.php
@@ -5,20 +5,31 @@ namespace App\Service;
 use App\DTO\LoginDTO;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AuthService
 {
     public function __construct(
-        private UserRepository $userRepository
+        private UserRepository $userRepository,
+        private UserPasswordHasherInterface $hasher
     ){}
 
-    public function login(LoginDTO $login)
+    public function login(LoginDTO $loginRequest)
     {
-        $login = $this->userRepository->findBy(['email' => $login->getEmail()]);
+        $loginRepository = $this->userRepository->findBy(['email' => $loginRequest->getEmail()]);
+        $isLoginEmpty = empty($loginRepository);
 
-        if (empty($login)) {
+        if ( $isLoginEmpty ) {
             throw new BadRequestException;
         }
-        return $login[0];
+
+        $user = $loginRepository[0];
+        $isPasswordValid = $this->hasher->isPasswordValid($user, $loginRequest->getPassword());
+
+        if ( !$isPasswordValid ) {
+            throw new BadRequestException;
+        }
+
+        return $user;
     }
 }
