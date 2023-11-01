0b915cd04fde4dd2f244868f78263003c3cc4d51
izadora matias <contato.izadoramatias@gmail.com>
2023-10-31 22:13:33 -0300

    catch http conflit exceptions

--- src/Controller/UserRegistrationController.php
+++ src/Controller/UserRegistrationController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserRegistrationController extends AbstractController
@@ -24,10 +25,18 @@ class UserRegistrationController extends AbstractController
         $register = UserRegisterDTO::fromRequest($request);
 
         try {
-            $userRegusterAuth = $authService->register($register);
+            $userRegisterAuth = $authService->register($register);
         } catch ( BadRequestException $exception ) {
             return $this->render(view: 'userRegisterFailed.html.twig', parameters: $this->registerData($register) , response: new Response(status: 400));
+        } catch ( ConflictHttpException $exception ) {
+            toastr()
+                ->closeOnHover(true)
+                ->closeDuration(10)
+                ->addError('O email informado já está cadastrado', 'Erro');
+            return $this->render(view: 'userRegisterFailed.html.twig', parameters: $this->registerData($register) , response: new Response(status: 409));
         }
+
+        return $this->render(view: 'userOrderList.html.twig', response: new Response(200));
     }
 
     private function registerData(UserRegisterDTO $userRegisterDTO): array
@@ -37,7 +46,6 @@ class UserRegistrationController extends AbstractController
             'email' => $userRegisterDTO->getEmail(),
             'phone' => $userRegisterDTO->getPhone(),
             'password' => $userRegisterDTO->getPassword(),
-            'passwordConfirmation' => $userRegisterDTO->getPasswordConfirmation(),
         ];
     }
 }
