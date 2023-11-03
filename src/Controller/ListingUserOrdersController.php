9fd0870ccc50fa890ed67e0bd66a297969b6e5b3
izadora matias <contato.izadoramatias@gmail.com>
2023-11-02 22:17:59 -0300

    adds session verification

--- src/Controller/ListingUserOrdersController.php
+++ src/Controller/ListingUserOrdersController.php
@@ -4,13 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ListingUserOrdersController extends AbstractController
 {
     #[Route('/home', name: 'app_user_order_listing_render', methods: ['GET'])]
-    public function renderListing(): Response
+    public function renderListing(Session $session): Response
     {
+        if ( !$session->has('isUserLogged') ){
+            $response = $this->redirectToRoute('app_login_render');
+            $response->setStatusCode(Response::HTTP_FORBIDDEN);
+
+            return $response;
+        }
+
         return $this->render('userOrderList.html.twig');
     }
 }
