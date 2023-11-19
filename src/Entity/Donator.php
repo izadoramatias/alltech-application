ab9359890d0701795689a4b47e53bc13c6306881
izadora matias <contato.izadoramatias@gmail.com>
2023-11-19 00:15:27 -0300

    create donator and equipment entities

---
+++ src/Entity/Donator.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DonatorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DonatorRepository::class)]
+class Donator
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $name = null;
+
+    #[ORM\OneToMany(mappedBy: 'donator', targetEntity: Equipment::class)]
+    private Collection $equipment;
+
+    public function __construct()
+    {
+        $this->equipment = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Equipment>
+     */
+    public function getEquipment(): Collection
+    {
+        return $this->equipment;
+    }
+
+    public function addEquipment(Equipment $equipment): static
+    {
+        if (!$this->equipment->contains($equipment)) {
+            $this->equipment->add($equipment);
+            $equipment->setDonator($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEquipment(Equipment $equipment): static
+    {
+        if ($this->equipment->removeElement($equipment)) {
+            // set the owning side to null (unless already changed)
+            if ($equipment->getDonator() === $this) {
+                $equipment->setDonator(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Equipment.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EquipmentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EquipmentRepository::class)]
+class Equipment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $receipt_date = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $availability = null;
+
+    #[ORM\ManyToOne(inversedBy: 'equipment')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Donator $donator = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getReceiptDate(): ?\DateTimeInterface
+    {
+        return $this->receipt_date;
+    }
+
+    public function setReceiptDate(\DateTimeInterface $receipt_date): static
+    {
+        $this->receipt_date = $receipt_date;
+
+        return $this;
+    }
+
+    public function getAvailability(): ?int
+    {
+        return $this->availability;
+    }
+
+    public function setAvailability(int $availability): static
+    {
+        $this->availability = $availability;
+
+        return $this;
+    }
+
+    public function getDonator(): ?Donator
+    {
+        return $this->donator;
+    }
+
+    public function setDonator(?Donator $donator): static
+    {
+        $this->donator = $donator;
+
+        return $this;
+    }
+}
